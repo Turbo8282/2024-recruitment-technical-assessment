b77521250c7ba04b28e87fe35af1826426c22869
Turbo8282 <alexwang2882@gmail.com>
2024-02-27 23:38:16 +1100

    unilectives fit

--- frontend/unilectives/src/pages/index.tsx
+++ frontend/unilectives/src/pages/index.tsx
@@ -14,10 +14,13 @@ export default function Home() {
         <div className="flex flex-row w-5/6 space-y-0 justify-between items-left md:space-y-4 md:flex-col md:items-center">
           <div className="flex flex-col w-full gap-3">
             <h1 className="drop-shadow-md text-base sm:text-sm">DevSoc presents</h1>
-            <div onClick={() => setUnilectives(unilectives => !unilectives)}>
-              {!unilectives && <h1 className="justify-center font-bold text-unilectives-color text-7xl sm:text-7xl">unilectives</h1>}
-              {unilectives && <h1 className="justify-center font-bold text-rose-400 text-7xl sm:text-7xl">unilectives</h1>}
+            <div className="w-fit">
+              <div onClick={() => setUnilectives(unilectives => !unilectives)}>
+                {!unilectives && <h1 className="justify-center font-bold text-unilectives-color text-7xl sm:text-7xl w-fit">unilectives</h1>}
+                {unilectives && <h1 className="justify-center font-bold text-rose-400 text-7xl sm:text-7xl w-fit">unilectives</h1>}
+              </div>
             </div>
+            
             <p className="justify-center font-bold text-base sm:text-sm">Your one-stop shop for UNSW course and elective reviews.</p>
             
             <SearchBar />
